a9195fd9ee377c693cfc1d4ef71ba669efee725c
Will Durkee <william.w.durkee@gmail.com>
2021-08-04 12:52:38 -0700

    updating files

---
+++ src/components/ScrollSide.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './ScrollSide.css'
+
+import {Container, Col, Row} from 'react-bootstrap'
+
+
+class ScrollSide extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+            <Container className="scroll-container">
+                <Row> 
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>  
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>    
+                    <Col><img src="gan-gif.gif" height="100" width="100" /> </Col>        
+                </Row>  
+            </Container>
+            </div>
+         );
+    }
+}
+ 
+export default ScrollSide;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './SideMenu.css'
+import { Image } from 'react-bootstrap';
+
+class SideMenu extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="side-menu">
+                <Image src="wilter-text.png" className="logo-image" />
+
+                <a href="https://open.spotify.com/artist/2Dhzt5rI1go7tAIIlTAz7R?si=ll-niLJZTlaUJyZDuMK58A&dl_branch=1" className="menu-item">
+                    <span className="menu-item-icon"><img src="plus.png" alt="" height="14" width="14"/></span>
+                    SPOTIFY
+                </a>
+                <a href="https://wilter.bandcamp.com/releases" className="menu-item">
+                    <span className="menu-item-icon"><img src="plus.png" alt="" height="14" width="14"/></span>
+                    BANDCAMP
+                </a>
+                <a href="https://www.instagram.com/wilt.er/" className="menu-item">
+                <span className="menu-item-icon"><img src="plus.png" alt="" height="14" width="14"/></span>
+                    INSTAGRAM
+                </a>
+                {/* <a href="https://www.tiktok.com/@_wilter" className="menu-item">
+                <span className="menu-item-icon"><img src="plus.png" alt="" height="14" width="14"/></span>
+                    TIK TOK
+                </a>
+                <a href="https://www.youtube.com/channel/UCPfgqlNC3FMaN1JsmsMAJPQ" className="menu-item">
+                <span className="menu-item-icon"><img src="plus.png" alt="" height="14" width="14"/></span>
+                    YOUTUBE
+                </a> */}
+                <p className="bottom-text">none of this was ever supposed to happen anyways.</p>
+                <p className="contact">mgmt@wilter.online</p>
+            </div>
+         );
+    }
+}
+ 
+export default SideMenu;

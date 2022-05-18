9baa95416f92571498c866ddd8854b150dad3b09
Will Durkee <william.w.durkee@gmail.com>
2022-05-17 17:39:40 -0700

    take out ticket link

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -9,7 +9,7 @@ class SideMenu extends React.Component {
     render() {
         return (<div className="side-menu" >
             <Image src="wilter-text.png" className="logo-image" />
-            <a href="https://dice.fm/partner/dice/event/pknrk-makeoutmusic-presents-wilter-12th-may-baader-house-los-angeles-tickets" className="menu-item" >
+            {/* <a href="https://dice.fm/partner/dice/event/pknrk-makeoutmusic-presents-wilter-12th-may-baader-house-los-angeles-tickets" className="menu-item" >
                 <span className="menu-item-icon" >
                     < img src="plus.png"
                         alt=""
@@ -17,8 +17,8 @@ class SideMenu extends React.Component {
                         width="14" />
                 </span>
                 EP RELEASE SHOW TICKETS
-            </a>
-            <a href="https://wilter.ffm.to/allthisleaving.OIS" className="menu-item" >
+            </a> */}
+            {/* <a href="https://wilter.ffm.to/allthisleaving.OIS" className="menu-item" >
                 <span className="menu-item-icon" >
                     <img src="plus.png"
                         alt=""
@@ -26,7 +26,7 @@ class SideMenu extends React.Component {
                         width="14" />
                 </span>
                 PRESAVE
-            </a>
+            </a> */}
             <a href="https://wilter.ffm.to/music" className="menu-item" >
                 <span className="menu-item-icon" >
                     <img src="plus.png"
